5dc884babe9412c0b084bfb1ed5906034980abb8
Sarthak Dwivedi <114822434+sarthak7772@users.noreply.github.com>
2025-06-14 15:33:03 +0530

    final commit

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useContext } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ApplyJob from './pages/ApplyJob';
+import Applications from './pages/Applications';
+import RecuiterLogin from './components/RecuiterLogin';
+import { AppContext } from './context/AppContext';
+import Dashboard from './pages/Dashboard';
+import AddJob from './pages/AddJob';
+import ManageJobs from './pages/ManageJobs';
+import ViewApplications from './pages/ViewApplications';
+import 'quill/dist/quill.snow.css' 
+function App() {
+  const { showRecruiterLogin } = useContext(AppContext)
+  return (
+    <div>
+      {showRecruiterLogin && <RecuiterLogin />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/apply-job/:id' element={<ApplyJob />} />
+        <Route path='/applications' element={<Applications />} />
+        <Route path='/dashboard' element={<Dashboard />}>
+        <Route path='add-job' element={<AddJob/>} />
+        <Route path='manage-jobs' element={<ManageJobs/>} />
+        <Route path='view-applications' element={<ViewApplications/>} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
